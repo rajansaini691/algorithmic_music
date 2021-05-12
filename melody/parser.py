e19a8ce6e9c0db834f185ea35a8e967d4cad905d
Rajan Saini <rajansaini@cs.ucsb.edu>
2021-05-11 17:27:48 -0700

    Add skeleton for frontend parser

---
+++ melody/parser.py
@@ -0,0 +1,20 @@
+
+"""
+Responsible for parsing a text file and converting to a Song object
+
+Example:
+    Key: C
+
+    Scale: 0 -> 3 -> 5 -> 8 -> 10           # Only this guy implemented for now
+        or
+    Scale: Major/major
+        or
+    Scale: C D Eb F G Ab Bb C
+
+    Phrase: *|F..*|F..*|Bb..*|F..*|C*Bb*|F
+        - Asterisk (*) denotes improvisation notes
+        - Note names, like F and Bb, denote landing notes
+           - Improvisation is based around these notes
+        - Periods (.) denote rests
+"""
+# TODO Implement this thing
