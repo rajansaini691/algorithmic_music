0a96388c3ea5ed98bea78efcd3df23a3933575b8
Rajan Saini <rajansaini@cs.ucsb.edu>
2021-05-20 12:10:45 -0700

    Reimplement Scale  - Called SimplerScale for now; will rename to Scale after some testing  - Way more effictive representation; works with non-Western scales,    like Indian Ragas and should work with half-flats (hasn't been tested    though)

--- melody/scale.py
+++ melody/scale.py
@@ -4,6 +4,7 @@ be shared between many melodies.
 """
 from dataclasses import dataclass
 from collections import namedtuple
+from helpers import midi_to_note_name
 import copy
 
 
@@ -19,9 +20,10 @@ def note_name_to_scale_degree(note_name):
         accidental = -1 if note_name[1] == 'b' else 1
         return lut[note_name] + accidental
 
-
+# TODO Transfer documentation onto SimplerScale
 class Scale:
-    def __init__(self, key, scale_degrees, same_up_down=True, octave_duplication=True):
+    def __init__(self, key, scale_degrees, name="[insert name]",
+                 same_up_down=True, octave_duplication=True):
         """
         Initialize scale with notes and rules.
         Example:
@@ -100,7 +102,6 @@ class Scale:
                 curr_node.note_below = prev_node
             scale_degree_nodes += [curr_node]
 
-            
         # Now copy the list multiple times and stitch together
         scale_notes = []
         for oct_num in range(10):
@@ -174,9 +175,105 @@ class Scale:
 
         return node.pitch
 
+
+class SimplerScale(Scale):
+    """
+    A newer, better implementation of Scale
+
+    To do C major, you must pass the equivalent of:
+        C D E F G A B C (the final C is required to show that B can go to C)
+    """
+    def __init__(self, key, scale_name, scale=None, ascending_scale=None, descending_scale=None):
+        assert((ascending_scale is not None) == (descending_scale is not None)), (
+                "If you're passing an ascending scale, you need a "
+                 "descending scale too, and vice-versa")
+        assert((descending_scale is not None) != (scale is not None)), (
+                "If your scale is the same going up and down, use scale "
+                "and leave ascending/descending as None")
+
+        self._scale_name = scale_name
+
+        self._ascending_scale = ascending_scale if not scale else scale
+        self._descending_scale = descending_scale if not scale else list(reversed(scale))
+
+    def _step_helper(self, current_note, scale):
+        octave = current_note // 12
+        notes = set()
+
+        # We subtract 1, since the last note in the scale shouldn't go to anything
+        for i in range(len(scale) - 1):
+            # sd stands for scale degree
+            sd = scale[i]
+            note = octave * 12 + sd
+
+            if note % 12 == current_note % 12:
+                num_steps = scale[i+1] - scale[i]
+                next_note = current_note + num_steps
+                notes.add(next_note)
+
+        assert(len(notes) > 0), f"In {self._scale_name}, you can't move up from {midi_to_note_name(current_note)}"
+        return notes
+
+    def step_up(self, current_note):
+        """
+        Returns a set of notes that can be played one-step above the current;
+        user may pick which one to use.
+        """
+        return self._step_helper(current_note, self._ascending_scale)
+
+    def step_down(self, current_note):
+        """
+        Returns a set of notes that can be played one-step below the current;
+        user may pick which one to use.
+        """
+        return self._step_helper(current_note, self._descending_scale)
+
+    def skip_up(self, current_note, n):
+        return self._ascending_scale.skip_up(current_note, n)
+
+    def skip_down(self, current_note, n):
+        return self.descending_scale.skip_down(current_note, n)
+
 if __name__ == "__main__":
     s = Scale('C', [0, 2, 4, 5, 7, 9, 11])
     assert(s.step_down(60) == 59)
     assert(s.step_up(60) == 62) # C -> D
     assert(s.step_up(62) == 64) # D -> E
     assert(s.step_up(64) == 65) # E -> F
+
+    # Test ascending-descending scale
+    yaman_arohana = [-1, 2, 4, 6, 7, 6, 9, 11, 12]
+    yaman_avarohana = [12, 11, 9, 7, 6, 4, 2, 0]
+    yaman = SimplerScale('C', scale_name="yaman", ascending_scale=yaman_arohana, descending_scale=yaman_avarohana)
+
+    # Test ascending
+    assert(yaman.step_up(59) == {60, 62})
+    assert(yaman.step_up(62) == {64})
+    assert(yaman.step_up(64) == {66})
+    assert(yaman.step_up(66) == {67, 69})
+    assert(yaman.step_up(67) == {66})
+    assert(yaman.step_up(69) == {71})
+    assert(yaman.step_up(71) == {72, 74})
+
+    # Test descending
+    assert(yaman.step_down(72) == {71}) # S -> N
+    assert(yaman.step_down(71) == {69}) # N -> Dh
+    assert(yaman.step_down(69) == {67}) # Dh -> P
+    assert(yaman.step_down(67) == {66}) # P -> M
+    assert(yaman.step_down(66) == {64}) # M -> G
+    assert(yaman.step_down(64) == {62}) # G -> R
+    assert(yaman.step_down(62) == {60}) # R -> S
+
+    # Test normal scale
+    C = SimplerScale('C', scale_name='C major', scale=[0, 2, 4, 5, 7, 9, 11, 12])
+
+    assert(C.step_up(60) == {62})   # C -> D
+    assert(C.step_down(62) == {60}) # D -> C
+    assert(C.step_up(62) == {64}) # D -> E
+    assert(C.step_down(64) == {62}) # E -> D
+    assert(C.step_up(64) == {65}) # E -> F
+    assert(C.step_down(65) == {64}) # F -> E
+    assert(C.step_up(65) == {67}) # F -> G
+    assert(C.step_down(67) == {65}) # G -> F
+    assert(C.step_up(64) == {65})   # B -> C
+    assert(C.step_down(65) == {64}) # C -> B
